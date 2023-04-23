19b66861b3634f0d529a70c084250b97c48c3cc4
EsiAmartey <49249453+EsiAmartey@users.noreply.github.com>
2023-04-23 01:42:18 -0500

    weather

--- index.js
+++ index.js
@@ -1,50 +1,86 @@
-const apiKey = "656f173395c28506763a3e9d15a312ad";
-const searchForm = document.getElementById("search-form");
-const searchInput = document.getElementById("search-input");
-const weatherInfo = document.getElementById("weather-info");
-const cityName = document.getElementById("city-name");
-const weatherIcon = document.getElementById("weather-icon");
-const temperature = document.getElementById("temperature");
-const description = document.getElementById("description");
-const humidity = document.getElementById("humidity");
-const windSpeed = document.getElementById("wind-speed");
-const forecastContainer = document.getElementById("forecast-container");
-const currentLocationBtn = document.getElementById("current-location-btn");
-
-// Search for a city
-searchForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const city = searchInput.value;
-  if (city) {
-    getWeatherData(city);
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
-});
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-// Get weather data for a city
-const getWeatherData = (city) => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-    .then((response) => response.json())
-    .then((data) => {
-      const { name, main, weather, wind } = data;
-      const { temp, humidity } = main;
-      const { description, icon } = weather[0];
-      const { speed } = wind;
-      cityName.textContent = `Current Weather in ${name}`;
-      weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/w/${icon}.png" alt="${description}">`;
-      temperature.textContent = `Temperature: ${temp} \u00B0C`;
-      description.textContent = `Weather: ${description}`;
-      humidity.textContent = `Humidity: ${humidity}%`;
-      windSpeed.textContent = `Wind Speed: ${speed} m/s`;
-      getForecastData(data.coord);
-    })
-    .catch((error) => console.log(error));
-};
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[dayIndex];
 
-// Get forecast data for a city
-const getForecastData = (coord) => {
-  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=metric`)
-    .then((response) => response.json())
-    .then((data) => {
-      const { daily } = data;
-      let forecastHTML = "";
-      daily.slice(1, 6
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "656f173395c28506763a3e9d15a312ad";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "656f173395c28506763a3e9d15a312ad";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Accra");
